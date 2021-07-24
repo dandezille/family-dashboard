8f6028629051b3c3f262cf01cfcd22d734c4ef99
Dan de Zille <dan@ddez.net>
2021-07-24 20:06:35 +0100

    Add controller for delete links

--- go/app/javascript/main.js
+++ go/app/javascript/main.js
@@ -1,10 +1,12 @@
 import ActivitiesController from './activities_controller.js'
+import LinkController from './link_controller.js'
 import TimeController from './time_controller.js'
 import WeatherController from './weather_controller.js'
 
 (() => {
   const application = Stimulus.Application.start()
   application.register("activities", ActivitiesController)
+  application.register("link", LinkController)
   application.register("time", TimeController)
   application.register("weather", WeatherController)
 })()
