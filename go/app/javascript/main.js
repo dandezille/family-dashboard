b448589757a2db4cee93e6385fb00e27f99a4da0
Dan de Zille <dan@ddez.net>
2021-07-24 20:03:35 +0100

    Sort controller imports

--- go/app/javascript/main.js
+++ go/app/javascript/main.js
@@ -1,10 +1,10 @@
-import TimeController from './time_controller.js'
 import ActivitiesController from './activities_controller.js'
+import TimeController from './time_controller.js'
 import WeatherController from './weather_controller.js'
 
 (() => {
   const application = Stimulus.Application.start()
-  application.register("time", TimeController)
   application.register("activities", ActivitiesController)
+  application.register("time", TimeController)
   application.register("weather", WeatherController)
 })()
